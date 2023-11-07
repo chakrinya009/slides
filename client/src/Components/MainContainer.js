acc4af5c02b329077d80bd9304c93586e0c5e653
chakrinya <chakrinyabalabadra@gmail.com>
2023-11-08 02:11:38 +0530

    clientside

--- client/src/Components/MainContainer.js
+++ client/src/Components/MainContainer.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import "./myStyles.css";
 import Sidebar from "./Sidebar";
-import WorkArea from "./WorkArea";
+import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-const MainContainer = () => {
+export const myContext = createContext();
+function MainContainer() {
+  const dispatch = useDispatch();
+  const lightTheme = useSelector((state) => state.themeKey);
+  const [refresh, setRefresh] = useState(true);
+
   return (
-    <div className="main-container">
-      <Sidebar />
-      <WorkArea />
+    <div className={"main-container" + (lightTheme ? "" : " dark")}>
+      <myContext.Provider value={{ refresh: refresh, setRefresh: setRefresh }}>
+        <Sidebar />
+        <Outlet />
+      </myContext.Provider>
+      {/* <Welcome /> */}
+      {/* <CreateGroups /> */}
+      {/* <ChatArea props={conversations[0]} /> */}
+      {/* <Users /> */}
+      {/* <Groups /> */}
     </div>
   );
-};
+}
 
 export default MainContainer;
